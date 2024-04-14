e272ceead2965e254a55eb21992f5d0f11cc7d00
Essam <essamwisam@outlook.com>
2024-04-14 23:41:19 +0200

    🎨 Better Suras list

--- Pages/HomePage/SurasList.tsx
+++ Pages/HomePage/SurasList.tsx
@@ -4,16 +4,17 @@ import { useNavigation } from "@react-navigation/native";
 // Components
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import ScrollBarView from "../Components/ScrollBar";
+// Helpers
+import { englishToArabicNumber, colorize } from "../../helpers";
 // State
 import { useDispatch, useSelector } from "react-redux";
 import {
   CurrentSurahInd,
-  SetCurrentSurahInd, 
+  SetCurrentSurahInd,
   JustEnteredNewSurah,
   SetJustEnteredNewSurah,
-  SetInHomePage 
+  SetInHomePage,
 } from "../../Redux/slices/app";
-
 
 interface Props {
   suras: any[];
@@ -37,7 +38,7 @@ const SurasList: React.FC<Props> = ({ suras }) => {
   ];
 
   // Set in HomePage as false once the user navigates out
-  const  setInHomePage = wrapDispatch(SetInHomePage);
+  const setInHomePage = wrapDispatch(SetInHomePage);
 
   return (
     <>
@@ -45,19 +46,62 @@ const SurasList: React.FC<Props> = ({ suras }) => {
         <View style={styles.container}>
           {suras.map((surah: any, index: number) => (
             <TouchableOpacity
-              style={styles.item}
+              style={styles.itemWrapper}
               key={index.toString()}
               onPress={() => {
                 if (index !== currentSurahInd) {
                   // to detect in audio player and go back to 1st Auya
                   setJustEnteredSurah(!justEnteredNewSurah);
                 }
-                setInHomePage(false)
+                setInHomePage(false);
                 setCurrentSurahInd(index);
                 navigation.navigate("SurahPage");
               }}
             >
-              <Text style={styles.title}>{surah.name}</Text>
+              <View style={styles.item}>
+                {/* contains khatim containing number then Surah Name */}
+                <View style={styles.surahAndNumberContainer}>
+                  <Text style={styles.khatim}>{"\ue901"}</Text>
+                  <View
+                    style={{
+                      position: "absolute",
+                      left: index < 10 ? 19 : 15,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        textAlign: "center",
+                        fontFamily: "UthmanBold",
+                        fontSize: 17,
+                        color: "white",
+                      }}
+                    >
+                      {englishToArabicNumber(index + 1)}
+                    </Text>
+                  </View>
+                  <View>
+                    <Text style={styles.title}>{"سُورَةُ " + surah.name}</Text>
+                  </View>
+                </View>
+                {/* contains the string with number of Ayas */}
+                <View style={{ position: "absolute", right: 90 }}>
+                  <Text
+                    style={[
+                      styles.title,
+                      { fontFamily: "UthmanRegular", fontSize: 20 },
+                    ]}
+                  >
+                    {englishToArabicNumber(surah.numAyas)}
+                    {surah.numAyas > 10 ? " آية" : " آيات"}
+                  </Text>
+                </View>
+                {/* contains whether surah is makeya or madaneya */}
+                <View>
+                  <Text style={styles.locIcon}>
+                    {surah.locType == 1 ? "\ue905" : "\ue902"}
+                  </Text>
+                </View>
+              </View>
             </TouchableOpacity>
           ))}
         </View>
@@ -68,30 +112,66 @@ const SurasList: React.FC<Props> = ({ suras }) => {
 
 const styles = StyleSheet.create({
   container: {
+    padding: 8,
     flex: 1,
-    borderColor: "black",
+    alignItems: "center",
     width: "100%",
+    justifyContent: "space-around",
+  },
+  containerStyle: {
+    flex: 1,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+
+    backgroundColor: colorize(-0.3, "#009193"),
+    padding: 6,
   },
   scrollViewWrapper: {
-    paddingHorizontal: 14,
     width: "100%",
     borderColor: "000",
   },
   scrollStyle: {
-    backgroundColor: "#38a3a5",
     opacity: 1.0,
+    backgroundColor: "#fff",
   },
-  item: {
-    backgroundColor: "#38a3a5",
+  surahAndNumberContainer: {
+    flexDirection: "row",
+    gap: 7,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  itemWrapper: {
     padding: 10,
     marginVertical: 4,
-    marginHorizontal: 12,
-    borderRadius: 10,
+    borderRadius: 30,
+    width: "100%",
+    borderBottomColor: "#009193",
+    borderBottomWidth: 2,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  item: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "95%",
+  },
+  khatim: {
+    fontFamily: "Khatim",
+    color: colorize(0.2, "#009193"),
+    fontSize: 48,
   },
   title: {
-    fontSize: 20,
+    fontSize: 23,
     fontFamily: "UthmanBold",
     color: "white",
+    letterSpacing: 4,
+  },
+  locIcon: {
+    fontFamily: "Khatim",
+    color: colorize(0.8, "#009193"),
+    fontSize: 40,
   },
 });
 
