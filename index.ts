e24526761fef13751cdb04d528c103b58a1608b9
Essam <essamwisam@outlook.com>
2024-06-12 22:56:40 -0500

    ✨ Much better font responsiveness

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 import { registerRootComponent } from 'expo';
+import {Text } from 'react-native';
 import TrackPlayer from 'react-native-track-player';
 import App from './App';
+
+// @ts-ignore
+Text.defaultProps = Text.defaultProps || {};
+// @ts-ignore
+Text.defaultProps.allowFontScaling = false;
 
 registerRootComponent(App);
 TrackPlayer.registerPlaybackService(() => require('./backgroundAudioService')); // for audio playing purposes
